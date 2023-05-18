268fce8f10d925763c4e8d07bc56b3f8bfcf1887
hieubui00.kma <hieubui00.kma@gmail.com>
2023-05-18 12:46:08 +0700

    feat: update prediction model for pe files

--- src/data/repository/windows_application.py
+++ src/data/repository/windows_application.py
@@ -2,19 +2,14 @@ import numpy as np
 
 from os import sep
 from os.path import join
-from typing import Annotated, Optional
-from fastapi import Depends
+from typing import Optional
 from pefile import PE
-from keras.models import load_model
+from pickle import load
 from src.data.util import extract, async_generator
 from src.domain.util import InvalidArgumentException
-from .model import ModelRepository
 
 
 class WindowsApplicationRepository:
-
-    def __init__(self, model_repository: Annotated[ModelRepository, Depends()]):
-        self._model_repository = model_repository
 
     async def create_application_analysis(self, pe_bytes: bytes) -> dict:
         try:
@@ -29,28 +24,22 @@ class WindowsApplicationRepository:
     async def _get_application_malware_type(self, pe_analytic: dict) -> Optional[str]:
         # Load model
         model_id = "644a35532a59a202605f6038"
-        model_source = join(sep, "data", "files", model_id, "model.h5")
-        model = load_model(filepath=model_source)
-
-        INPUT_SIZE = 9
+        model = self._get_model(model_id=model_id)
 
         # Pre-processing
         model_input = [
-            'e_magic', 'e_cblp', 'e_cp', 'e_crlc', 'e_cparhdr', 'e_minalloc', 'e_maxalloc', 'e_ss', 'e_sp',
-            'e_csum', 'e_ip', 'e_cs', 'e_lfarlc', 'e_ovno', 'e_oemid', 'e_oeminfo', 'e_lfanew', 'Machine',
-            'SizeOfOptionalHeader', 'Characteristics', 'Signature', 'Magic', 'MajorLinkerVersion',
-            'MinorLinkerVersion', 'SizeOfCode', 'SizeOfInitializedData', 'SizeOfUninitializedData',
-            'AddressOfEntryPoint', 'BaseOfCode', 'BaseOfData', 'ImageBase', 'SectionAlignment',
-            'FileAlignment', 'MajorOperatingSystemVersion', 'MinorOperatingSystemVersion',
-            'MajorImageVersion', 'MinorImageVersion', 'MajorSubsystemVersion', 'MinorSubsystemVersion',
-            'Reserved1', 'SizeOfImage', 'SizeOfHeaders', 'CheckSum', 'Subsystem', 'DllCharacteristics',
-            'SizeOfStackReserve', 'SizeOfStackCommit', 'SizeOfHeapReserve', 'SizeOfHeapCommit', 'LoaderFlags',
-            'NumberOfRvaAndSizes', 'LengthOfPeSections', 'MeanEntropy', 'MinEntropy', 'MaxEntropy',
-            'MeanRawSize', 'MinRawSize', 'MaxRawSize', 'MeanVirtualSize', 'MinVirtualSize', 'MaxVirtualSize',
-            'ImportsNbDLL', 'ImportsNb', 'ImportsNbOrdinal', 'ExportNb', 'ResourcesMeanEntropy',
-            'ResourcesMinEntropy', 'ResourcesMaxEntropy', 'ResourcesMeanSize', 'ResourcesMinSize',
-            'ResourcesMaxSize', 'ResourcesNb', 'LoadConfigurationSize', 'VersionInformationSize', 'DLL',
-            'LengthOfInformation'
+            'e_magic', 'e_cblp', 'e_cp', 'e_crlc', 'e_cparhdr', 'e_minalloc', 'e_maxalloc', 'e_ss', 
+            'e_sp', 'e_csum', 'e_ip', 'e_cs', 'e_lfarlc', 'e_ovno', 'e_oemid', 'e_oeminfo', 
+            'e_lfanew', 'Machine', 'SizeOfOptionalHeader', 'Characteristics', 'Signature', 
+            'Magic', 'MajorLinkerVersion', 'MinorLinkerVersion', 'SizeOfCode', 
+            'SizeOfInitializedData', 'SizeOfUninitializedData', 'AddressOfEntryPoint', 'BaseOfCode', 
+            'BaseOfData', 'ImageBase', 'SectionAlignment', 'FileAlignment', 
+            'MajorOperatingSystemVersion', 'MinorOperatingSystemVersion', 'MajorImageVersion', 
+            'MinorImageVersion', 'MajorSubsystemVersion', 'MinorSubsystemVersion', 'Reserved1', 
+            'SizeOfImage', 'SizeOfHeaders', 'CheckSum', 'Subsystem', 'DllCharacteristics', 
+            'SizeOfStackReserve', 'SizeOfStackCommit', 'SizeOfHeapReserve', 'SizeOfHeapCommit', 
+            'LoaderFlags', 'NumberOfRvaAndSizes', 'LengthOfPeSections', 'MeanRawSize', 
+            'MeanVirtualSize', 'ImportsNbDLL', 'ImportsNbOrdinal'
         ]
         size = len(model_input)
         buffer = [0] * size
@@ -61,23 +50,26 @@ class WindowsApplicationRepository:
                 buffer[i] = pe_analytic[key]
             except:
                 pass
-
         x = np.array(object=[buffer])
         x = np.nan_to_num(x=x, nan=0)
-        x = x / np.max(x, axis=0)
-        x = np.concatenate((x, np.zeros((x.shape[0], 5))), 1)
-        x = x.reshape(x.shape[0], INPUT_SIZE, INPUT_SIZE, 1)
 
         # Run model prediction with the input data.
-        y = model(x)[0]
-        y = list(y)
+        y = model.predict(x)[0]
 
-        # Post-processing: find the digit that has the highest probability
-        model_output = ["Benign", "Malware"]
-        highest_probability = max(y)
-        index = y.index(highest_probability)
+        # Post-processing:
+        model_output = [
+            "Benign", "Virus", "Trojan", "Adware", "Worm", "Pua", "Downloader", "Ransomware", 
+            "Hacktool", "Miner", "Banker", "No Label", "Dropper", "Fakeav", "Suspack", "Damaged"
+        ]
+        result = model_output[y]
+        return result
 
-        return model_output[index]
+    def _get_model(self, model_id: str):
+        model_source = join(sep, "data", "files", model_id, "model.pickle")
+
+        with open(file=model_source, mode='rb') as file:
+            model = load(file=file)
+        return model
 
     async def get_application_analysis(self, analysis_id: str) -> Optional[dict]:
         return None
