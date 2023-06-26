103cb04961cbfb088e44b741a041b16136c17724
hieubui00.kma <hieubui00.kma@gmail.com>
2023-06-26 10:25:57 +0700

    refactor(model): separate code and optimize code

---
+++ src/data/local/document/model.py
@@ -0,0 +1,38 @@
+from src.domain.data.model import Model, ModelDetails, ModelHistory
+
+
+def as_model(document) -> Model:
+    return Model(
+        id=str(object=document['_id']),
+        version=document['version'],
+        type=document['type'],
+        input_format=document['input_format'],
+        created_at=document['created_at'].isoformat()
+    )
+
+
+def as_model_details(document, source_size: int) -> ModelDetails:
+    return ModelDetails(
+        id=str(object=document['_id']),
+        version=document['version'],
+        type=document['type'],
+        size=source_size,
+        input_format=document['input_format'],
+        datasets=document['datasets'],
+        output=document['output'],
+        accuracy=document['accuracy'],
+        loss=document['loss'],
+        precision=document['precision'],
+        recall=document['recall'],
+        f1=document['f1'],
+        created_at=document['created_at'].isoformat()
+    )
+
+
+def as_model_history(document) -> ModelHistory:
+    return ModelHistory(
+        accuracy=document['accuracy'],
+        val_accuracy=document['val_accuracy'],
+        loss=document['loss'],
+        val_loss=document['val_loss'],
+    )
